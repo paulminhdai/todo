810a0865c09e0850f0f1ff58cc43f68d81222770
daivuong <paulminhdai@gmail.com>
2021-01-17 14:05:13 -0800

    fix database

--- todo/settings.py
+++ todo/settings.py
@@ -75,15 +75,11 @@ WSGI_APPLICATION = 'todo.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'DEMO_TEST',
-        'USER': 'postgres',
-        'PASSWORD': 'IamkinG!58',
-        'HOST': 'localhost',
-        'PORT': '5432'
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
 
 
 # Password validation
